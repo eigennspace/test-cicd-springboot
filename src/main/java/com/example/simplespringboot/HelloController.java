9e4e3c4850c6aed364ed6b9a7bac05fc78912ccf
Harist Islami <islamiharist1998@gmail.com>
2023-01-20 19:49:38 +0700

    Initial Commit

---
+++ src/main/java/com/example/simplespringboot/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.simplespringboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World CI/CD";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String helloParameterize(@PathVariable("name") String name){
+        log.info("Say hello to {}", name);
+        return "Hello " + name + " how are you";
+    }
+}
